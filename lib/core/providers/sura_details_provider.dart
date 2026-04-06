8af6177bc5e6fe2ff3cfdc3a2373c2dae80c1dae
Nada Ali <96360993+nada-ali1711@users.noreply.github.com>
2026-04-06 14:08:28 +0200

    edited sura details provider as it did not reset the content it appends every sura content so edited it + make reusable golden_circular_progress_indicator.dart and show it in quran details while loading + added if condition to prevent calling load data function inside didchangedependencies funsion so if we are inside the same page by checking current index is the same so do not call the function

--- lib/core/providers/sura_details_provider.dart
+++ lib/core/providers/sura_details_provider.dart
@@ -12,14 +12,20 @@ class SuraDetailsProvider extends ChangeNotifier {
   }
 
   Future<void> loadFileData(int index) async {
+    suraContent = '';
+    suraVerses = [];
+    String tempContent = '';
+
     String content = await rootBundle.loadString(
       'assets/suras/${index + 1}.txt',
     );
     List<String> verses = content.trim().split('\n');
-    suraVerses = verses;
-    for (int i = 0; i < suraVerses.length; i++) {
-      suraContent += '[${i + 1}] ${suraVerses[i]}';
+
+    for (int i = 0; i < verses.length; i++) {
+      tempContent += '[${i + 1}] ${verses[i]}';
     }
+    suraContent = tempContent;
+    suraVerses = verses;
 
     notifyListeners();
   }
